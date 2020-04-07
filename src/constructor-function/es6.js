d7ba49ee3ebc7e28ba75a6f5162b429410933862
Rahul Singh <50801861+rsdoc@users.noreply.github.com>
2020-04-07 14:01:43 +0530

    es6 - constructor function

--- src/constructor-function/es6.js
+++ src/constructor-function/es6.js
@@ -1,1 +1,16 @@
 console.log("[es6 constructor function]");
+
+// es6 way of creating constructor function
+class Alert {
+  constructor(title) {
+    this.title = title || "Default Alert";
+  }
+
+  logData() {
+    console.log("[Logger - Alert - log Data] - ", this.title);
+  }
+}
+
+const alert = new Alert("Success");
+
+console.log(alert.logData());
