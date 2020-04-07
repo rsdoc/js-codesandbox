d9c78e758aca8f9cdcfd55f3395aff6aca9e6068
Rahul Singh <50801861+rsdoc@users.noreply.github.com>
2020-04-07 17:31:45 +0530

    es5 inheritance

--- src/02-inheritance/es5.js
+++ src/02-inheritance/es5.js
@@ -1,31 +1,15 @@
 console.log("[ es5 inheritance ]");
 
-/**
- * By convention - keep function name as Capitalize.
- * Scope of this - will be Alert constructor function
- * By default - if no return is specified - it returns instance of Alert costructor function
- */
+// creating base constructor function
 function Alert(title) {
-  // adding guard to return instance of Alert
-  // incase someone called this as -
-  // const alert = Alert("Success") i.e normal function call
-  // make use of prototype
+  
   if (!(this instanceof Alert)) {
     return new Alert(title);
   }
+
   this.title = title || "Default Alert";
-
-  // adding functions
-  // we should avoid using functions here - to not allocated memory to this function on every intantiation
-
-  /**
-   this.logData = function() {
-    console.log("[Logger - Alert - log Data line] - ", this.title);
-  };
-   */
 }
 
-// all istances will have ref to this method
 Alert.prototype.logData = function() {
   console.log("[ logData - (Alert) ] - ", this.title);
 };
@@ -34,8 +18,14 @@ Alert.prototype.toString = function() {
   return `[ toString - (Alert) ] - ${this.title}`;
 };
 
+// creating derived constructor function
+function SuccessAlert() {
+  this.type = "success";
+}
+
+
 // creating instance of constructor function
-const alert = new Alert("Success toast");
+const alert = new Alert("Default Alert");
 
 console.log(alert.logData());
 console.log(alert.toString());
