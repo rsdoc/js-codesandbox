85457de72258f1638e857e7dc5ccab2c3842f7d6
Rahul Singh <50801861+rsdoc@users.noreply.github.com>
2020-04-07 20:55:56 +0530

    finished override method

--- src/03-method-overrride/es5.js
+++ src/03-method-overrride/es5.js
@@ -1,5 +1,10 @@
 console.log("[ es5 method override ]");
 
+/**
+ * 1. override the method of Base class
+ * 2. Call the method of Base class from derived class
+ */
+
 function Alert(title) {
   console.log(" [ Alert ] - constructor function");
   this.title = title || "Default Alert";
@@ -25,6 +30,28 @@ SuccessAlert.prototype = Object.create(Alert.prototype);
 
 SuccessAlert.prototype.constructor = SuccessAlert;
 
+// creating FailureAlert derived class
+function FailureAlert(title) {
+  Alert.call(this, title);
+
+  this.type = "Failure";
+
+  console.log(" [ FailureAlert ] - constructor function");
+}
+
+FailureAlert.prototype = Object.create(Alert.prototype);
+
+FailureAlert.prototype.constructor = FailureAlert;
+
+// method overriding of Alert in FailureAlert
+FailureAlert.prototype.toString = function() {
+  // making call to parent constructor function
+  const result = Alert.prototype.toString.call(this);
+  return `${result} of type ${this.type}`;
+};
+
 const sa = new SuccessAlert("Sucess from SA");
+const fa = new FailureAlert("Failure from FA");
 
 console.log(sa.toString());
+console.log(fa.toString());
--- src/03-method-overrride/es6.js
+++ src/03-method-overrride/es6.js
@@ -21,6 +21,24 @@ class SuccessAlert extends Alert {
   }
 }
 
+// creating derived class
+class FailureAlert extends Alert {
+  constructor(title) {
+    super(title);
+    this.type = "Failure";
+  }
+
+  toString() {
+    let result = super.toString();
+
+    console.log(result);
+
+    return `${result} of type ${this.type}`;
+  }
+}
+
 const sa = new SuccessAlert("Success Alert");
+const fa = new FailureAlert("Failure Alert");
 
 console.log(sa.toString());
+console.log(fa.toString());
